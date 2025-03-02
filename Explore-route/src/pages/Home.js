650382eb11b2f71fc6f8ff36fc1944b4bf6148cf
Ahmed Aladdin <ahmed3laa3132@gmail.com>
2025-03-02 20:22:07 +0200

    react-router-dom | navigating programmatically

--- Explore-route/src/pages/Home.js
+++ Explore-route/src/pages/Home.js
@@ -1,10 +1,17 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function HomePage (){
+    const navigate = useNavigate();
+
+    function navigateToProducts(){
+        navigate('/products');
+    }
+
     return (
         <div>
             <h1>Home Page</h1>
             <p>Go to <Link to="/products">products page</Link>.</p>
+            <button onClick={navigateToProducts}>Go to Products</button>
         </div>
     )
 }
