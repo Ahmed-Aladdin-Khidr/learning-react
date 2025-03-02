9339f20e29b453261b0d994e6c6c6d72dc9f37f2
Ahmed Aladdin <ahmed3laa3132@gmail.com>
2025-03-02 17:34:08 +0200

    use Link instead of <a>

--- Explore-route/src/pages/Home.js
+++ Explore-route/src/pages/Home.js
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom';
+
 function HomePage (){
     return (
         <div>
             <h1>Home Page</h1>
+            <p>Go to <Link to="/products">products page</Link>.</p>
         </div>
     )
 }
