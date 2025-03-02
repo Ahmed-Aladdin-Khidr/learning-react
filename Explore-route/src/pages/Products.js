a4278e15db76786a713d75ab0896bff518a355eb
Ahmed Aladdin <ahmed3laa3132@gmail.com>
2025-03-02 20:28:34 +0200

    react-router-dom | defining & using dynamic routes/ using params

--- Explore-route/src/pages/Products.js
+++ Explore-route/src/pages/Products.js
@@ -2,6 +2,11 @@ function ProductsPage(){
     return (
         <div>
             <h1>Products Page</h1>
+            <ul>
+                <li>Product 1</li>
+                <li>Product 2</li>
+                <li>Product 3</li>
+            </ul>
         </div>
     );
 }
