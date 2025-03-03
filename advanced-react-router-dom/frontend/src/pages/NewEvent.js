3aa24f76d72376e9960e723f9be99f1611a73a37
Ahmed Aladdin <ahmed3laa3132@gmail.com>
2025-03-04 00:13:05 +0200

    Implement event creation functionality in NewEventPage with form handling and redirection

--- advanced-react-router-dom/frontend/src/pages/NewEvent.js
+++ advanced-react-router-dom/frontend/src/pages/NewEvent.js
@@ -1,9 +1,34 @@
-function NewEventPage(){
-    return (
-        <div>
-            <h1>New Event Page</h1>
-        </div>
-    );
+import { redirect } from "react-router-dom";
+import EventForm from "../components/EventForm";
+
+function NewEventPage() {
+  return <EventForm />;
 }
 
 export default NewEventPage;
+
+export async function action({ request, params }) {
+  const data = await request.formData();
+
+  const eventDate = {
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
+  };
+
+  const response = await fetch("http://localhost:8080/events", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventDate),
+  });
+
+  if (!response.ok) {
+    throw new Response(JSON.stringify({ message: "Failed to create event" }), {
+      status: 500,
+    });
+  }
+    return redirect("/events");
+}
