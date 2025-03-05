b2ea73c7604690c5f6e4e43078d23e1b28197b28
Ahmed Aladdin <ahmed3laa3132@gmail.com>
2025-03-05 23:38:21 +0200

    Add newsletter signup feature with form handling using useFetcher

--- advanced-react-router-dom/frontend/src/App.js
+++ advanced-react-router-dom/frontend/src/App.js
@@ -33,6 +33,7 @@ import RootLayout from "./pages/Root";
 import EventsRootLayout from "./pages/EventsRoot";
 import ErrorPage from "./pages/Error";
 import {action as manipulateEventAction } from './components/EventForm'
+import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 
 const router = createBrowserRouter([
   {
@@ -61,6 +62,11 @@ const router = createBrowserRouter([
           },
           { path: "new", element: <NewEventPage />, action: manipulateEventAction },
         ],
+      },
+      {
+        path: 'newsletter',
+        element: <NewsletterPage />,
+        action: newsletterAction,
       },
     ],
   },
